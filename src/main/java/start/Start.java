6ab668c868a878bcac36f2aee00edf213964c47d
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2022-02-15 22:15:43 +0100

    v0.0.2

--- src/main/java/start/Start.java
+++ src/main/java/start/Start.java
@@ -1,11 +1,10 @@
-package hellocucumber;
+package start;
 
 import utils.Printer;
 
 public class Start {
-
     public static void main(String[] args) {
         Printer log = new Printer(Start.class);
-        log.new important("Welcome to the Oasis.");
+        log.new Important("Welcome to the Oasis.");
     }
 }
