eb4dea1fce82d1dc931c31ee8a4ce8f230631d5d
Umut Ay Bora <umut.ay.bora@spritecloud.com>
2022-02-15 20:36:32 +0100

    Initial artifact fix

---
+++ src/test/java/hellocucumber/Start.java
@@ -0,0 +1,11 @@
+package hellocucumber;
+
+import utils.Printer;
+
+public class Start {
+
+    public static void main(String[] args) {
+        Printer log = new Printer(Start.class);
+        log.new important("Welcome to the Oasis.");
+    }
+}
